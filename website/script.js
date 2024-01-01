baa51d5783b341782640cb726fd421c8bd7d61c7
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-01-01 13:07:34 -0700

    updated styling

--- website/script.js
+++ website/script.js
@@ -1,16 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.body.style.opacity = 1;
 
-  setTimeout(function () {
-    document.querySelector('header').classList.add('fade-in');
-  }, 500);
+  const header = document.querySelector('header');
+
+  // Check if the header has not already been faded in
+  if (!header.classList.contains('fade-in')) {
+    setTimeout(function () {
+      header.classList.add('fade-in');
+
+      // Remove the fade-in class after a delay (e.g., 2000 milliseconds)
+      setTimeout(function () {
+        header.classList.remove('fade-in');
+      }, 100);
+    }, 100);
+  }
 
   setTimeout(function () {
     document.getElementById('main-container').classList.add('fade-in');
   }, 1000);
 
+  setTimeout(function () {
+    document.querySelector('footer').classList.add('fade-in');
+  }, 500);
+
   // Fix typos in the following lines
-  const footerContent = document.getElementById("footer-content");
+  const footerContent = document.getElementById('footer-content');
   const currentYear = new Date().getFullYear();
-  footerContent.textContent += ` ${currentYear}`;
+  footerContent.textContent += `©Kanuck Studios ${currentYear}`;
 });
